6a5fac5ecc08fc71e097a58d3af05548dd1f2aea
Viraj Abeywardana <abeywardana21@gmail.com>
2023-12-16 10:29:45 +0530

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'First Flutter App
-      
-      '),
+      home: const MyHomePage(title: 'First Flutter App'),
     );
   }
 }
