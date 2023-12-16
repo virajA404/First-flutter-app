4df6a3be694de1da1d6f6d8febda56d9e8adbb02
Viraj Abeywardana <abeywardana21@gmail.com>
2023-12-16 09:43:14 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'First Flutter App
+      
+      '),
     );
   }
 }
